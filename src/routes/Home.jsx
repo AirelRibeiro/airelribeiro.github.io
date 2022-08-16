ecaf5aff6229c91fade25515b5fe5df1a5ada70e
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-08-16 20:09:26 -0300

    feat [Home.jsx]: includes Home page

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,7 +1,22 @@
+import '../style/Home.css';
+
 function Home() {
   return (
-    <p>Home</p>
-    
+    <div className="home">
+      <h1>Oi, eu sou Airel!</h1>
+      <div className="descricao">
+        <p>
+          Apaixonade por palavras, informação e tecnologia;
+          também acredito em dar suporte e apoiar pessoas.
+        </p>
+        <p>
+          Vejo nas tecnologias de informação e comunicação incríveis possibilidades de impactar
+          positivamente a sociedade e tranformar a vida das pessoas.
+          Por isso estudo para me tornar desenvolvedore Full Stack e
+          participar ativamente dessa transformação.
+        </p>
+      </div>
+    </div>
   );
 }
 
