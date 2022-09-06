d13b9be690fa601e330b8f917730ee450f693c14
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-09-06 20:18:58 -0300

    feat: creates Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+  return (
+    <div className="header">
+      <Link to="/">
+        <img
+          src="https://img.icons8.com/nolan/512/home.png"
+          alt="home-icon"
+          className="header-icons"
+        />
+      </Link>
+      <Link to="/skills">
+        <img
+          src="https://img.icons8.com/nolan/512/maintenance.png"
+          alt="skills-icon"
+          className="header-icons"
+        />
+      </Link>
+      <Link to="/projects">
+        <img
+          src="https://img.icons8.com/nolan/512/code--v2.png"
+          alt="projects-icon"
+          className="header-icons"
+        />
+      </Link>
+      {/* <a href="../helpers/airel-curriculo.pdf" download>
+        <img
+          src="https://img.icons8.com/nolan/512/open-resume.png"
+          alt="cv-icon"
+          className="header-icons"
+        />
+      </a> */}
+      <Link to="/contact">
+        <img
+          src="https://img.icons8.com/nolan/512/apple-contacts.png"
+          alt="contact-icon"
+          className="header-icons"
+        />
+      </Link>
+    </div>
+  );
+}
