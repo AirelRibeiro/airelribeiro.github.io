c9fce8add9e2de59310505ac40bddc2e15736bdc
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-09-23 19:02:13 -0300

    feat: includes projects in page

--- src/routes/Projects.jsx
+++ src/routes/Projects.jsx
@@ -1,16 +1,46 @@
-import hammer from '../icons/hammer.svg';
+import Carousel from 'react-bootstrap/Carousel';
 import '../style/Projects.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import projects from '../helpers/projectsData';
 
 function Projects() {
   return (
-    <div className="projects">
-      <h1>Página em construção...</h1>
-      <img
-        src={hammer}
-        alt="git-icon"
-        className="hammer-icon"
-      />
-    </div>
+    <>
+      {/* <h1 className="projects-title">Meus projetos</h1> */}
+      <div className="projects">
+        <Carousel className="carousel">
+          {projects.map((p) => (
+            <Carousel.Item interval={2500}>
+              <div className="items">
+                <img
+                  className={p.c ? 'hammer' : 'image'}
+                  src={p.icon}
+                  alt={p.name}
+                />
+
+                <div className="informations">
+                  <h2 className="title">{p.name}</h2>
+                  <p className="project-description">{p.description}</p>
+                  <div className="tecnologies-div">
+                    {p.tecnologies.map((t) => (
+                      <img
+                        className="tecnology"
+                        src={t}
+                        alt="icon"
+                      />
+                    ))}
+                  </div>
+                  <a href={p.repository} target="_blank" rel="noreferrer">Acesse aqui o repositório desse projeto</a>
+
+                </div>
+              </div>
+            </Carousel.Item>
+          ))}
+
+        </Carousel>
+      </div>
+
+    </>
   );
 }
 
